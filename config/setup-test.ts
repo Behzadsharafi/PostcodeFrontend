a9c9ddb0cd4b947ceacf46e2328972498902af42
Behzad Sharafi <behzadsharafi@gmail.com>
2024-01-04 15:14:37 +1100

    adds test badge

--- config/setup-test.ts
+++ config/setup-test.ts
@@ -1,6 +1,6 @@
+import "@testing-library/jest-dom";
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
 
 // clean up after every test - unmount my components
 afterEach(() => {
--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./config/setup-test",
+    setupFiles: "./config/setup-test.ts",
+    css: true,
   },
 });
